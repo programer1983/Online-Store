a82e613a56d4def26d509385c85cf91c03b62541
programer1983 <programer972@gmail.com>
2022-02-09 10:27:49 +0200

    routing;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from "./components/Navbar"
+import Products from "./components/Products"
+import {Routes, Route} from "react-router-dom"
+ 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+    <Navbar />
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/products' element={<Products/>}/>
+    </Routes>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,44 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
-const navbar = () => {
+
+const Navbar = () => {
 return (
 <div>
 <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
   <div class="container">
-    <a class="navbar-brand fw-bold fs-4" href="#" >COLLECTION</a>
+    <Link class="navbar-brand fw-bold fs-4" to="/" >COLLECTION</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Products</a>
+          <Link class="nav-link" to="/products">Products</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <Link class="nav-link" to="/about">About</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <Link class="nav-link" to="/contact">Contact</Link>
         </li>
       </ul>
     <div className="buttons">
-        <a href="" className="btn btn-outline-dark">
+        <Link to="/login" className="btn btn-outline-dark">
             <i className="fa fa-sign-in me-2"></i>
             Login
-        </a>
-        <a href="" className="btn btn-outline-dark ms-2">
+        </Link>
+        <Link to="/register" className="btn btn-outline-dark ms-2">
             <i className="fa fa-user-plus me-2"></i>
             Register
-        </a>
-        <a href="" className="btn btn-outline-dark ms-2">
+        </Link>
+        <Link to="/cart" className="btn btn-outline-dark ms-2">
             <i className="fa fa-shopping-cart me-2"></i>
             Cart(0)
-        </a>
+        </Link>
     </div>
     </div>
   </div>
@@ -45,4 +47,4 @@ return (
   )
 };
 
-export default navbar;
+export default Navbar;
